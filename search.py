9d138b76416131a81013da0595724b04a731201e
Mackenzie Frew <11793063+frewmack@users.noreply.github.com>
2022-11-19 13:48:40 -0500

    Add code for parsing CSV file

--- search.py
+++ search.py
@@ -1,4 +1,4 @@
-import os
+import os, csv
 
 # import spotify.sync as spotify
 import click
@@ -20,15 +20,33 @@ PLAYLIST_EXPORT_URL = "https://www.spotlistr.com/export/spotify-playlist"
 # def query_bar_plus():
 #     pass
 
+ARTIST = 0
+TRACK = 1
+
 @click.command()
-@click.option("--method", type=click.Choice(["csv"]), prompt=True, case_sensitive=False, required=True)
-@click.argument("csv_path", type=click.Path(exists=True), prompt=True)
-def main(method, csv_path):
-    # 1 Obtain song list
-    songs = []
-    artists = []
+@click.option("--method", type=click.Choice(["csv"]), required=True)
+@click.option("--csv-path", type=click.Path(exists=True))
+@click.option("--query-by", type=click.Choice(["track", "author"]), required=True)
+def main(method, csv_path, query_by):
+    # Build dict, artist -> songs
+    click.echo("Compiling artist and song lists...")
+    artists_songs = {}
     if method == "csv":
-        pass
+        with open(csv_path, newline='', encoding="utf-8") as f:
+            reader = csv.reader(f, delimiter=",")
+            fields = next(reader)  # skip field names
+            count = 0
+            for i, row in enumerate(reader):
+                if row[ARTIST] not in artists_songs:
+                    artists_songs[row[ARTIST]] = [row[TRACK]]
+                else:
+                    artists_songs[row[ARTIST]].append(row[TRACK])
+                count += 1
+            
+            click.echo(f"Lists compiled. {len(artists_songs)} total artists, {count} total songs")
+    
+    
+    # Query
 
 
 if __name__ == "__main__":
