dcb608672bd30dd7e37afa9f439296ece48eda4c
Mackenzie Frew <11793063+frewmack@users.noreply.github.com>
2022-11-23 01:44:32 -0500

    Fix lowercase titles in output files - Update .gitignore

--- search.py
+++ search.py
@@ -35,6 +35,8 @@ def query_bar_plus(method: str, field: str, page=1) -> str:
     """
     Queries the Bar+ website for the particular field, and returns the resulting HTML.
     """
+    field = field.lower()
+
     params = {
         "Song[Name]": "",
         "Song[Artist]": "",
@@ -72,13 +74,13 @@ def parse_bar_plus_html(html: str, page: int) -> Tuple[Dict[str, BarPlusSong], O
     if len(songs) == 1 and len(songs[0]) < 4:
         # there are no songs
         return {}, None
-    songs = [BarPlusSong(song[1].strip().lower(), song[2].strip().lower(), song[3].strip(), song[0].strip()) for song in songs]
+    songs = [BarPlusSong(song[1].strip(), song[2].strip(), song[3].strip(), song[0].strip()) for song in songs]
     artists_songs = {}
     for song in songs:
-        if song.artist not in artists_songs:
-            artists_songs[song.artist] = [song]
+        if song.artist.lower() not in artists_songs:
+            artists_songs[song.artist.lower()] = (song.artist, [song])
         else:
-            artists_songs[song.artist].append(song)
+            artists_songs[song.artist.lower()][TRACK].append(song)
 
     # get last page
     li_tag = soup.find("li", attrs={"class": "last"})
@@ -106,16 +108,16 @@ def query_songs_by_artist(artist: str) -> List[BarPlusSong]:
     while page is not None:
         html = query_bar_plus("artist", artist, page)
         songs, next_page = parse_bar_plus_html(html, page)
-        if len(songs) == 0 or songs.get(artist) is None:
+        if len(songs) == 0 or songs.get(artist.lower()) is None:
             # no songs
             return []
-        songs = songs[artist]  # dictionary
+        songs = songs[artist.lower()][TRACK]  # dictionary
         all_songs.extend(songs)
         page = next_page
     return all_songs
 
 
-def organize_found_songs(wanted_songs: List[str], found_songs: List[BarPlusSong]) -> Tuple[List[BarPlusSong], List[BarPlusSong], List[str]]:
+def organize_found_songs(wanted_songs: List[str], found_songs: List[BarPlusSong]) -> Tuple[List[BarPlusSong], List[BarPlusSong], List[Tuple[str, str]]]:
     """
     Organize songs found by a certain artist.
     Return 3 lists representing:
@@ -127,20 +129,20 @@ def organize_found_songs(wanted_songs: List[str], found_songs: List[BarPlusSong]
     - wanted_songs and found_songs are all by the same artist
     """
 
-    wanted_songs_set = set(wanted_songs)
+    wanted_songs_dict = dict([(song.lower(), i) for i, song in enumerate(wanted_songs)])
 
     requested, bonus, missing = [], [], []
     for song in found_songs:
-        if song.name in wanted_songs_set:
+        if song.name.lower() in wanted_songs_dict:
             # Found one that we wanted!
             requested.append(song)
             # Bar+ sometimes has duplicates, but we only look at the first occurrence
-            wanted_songs_set.discard(song.name)
+            wanted_songs_dict.pop(song.name.lower())
         else:
             # Bonus!
             bonus.append(song)
 
-    missing = list(wanted_songs_set)
+    missing = [wanted_songs[i] for i in wanted_songs_dict.values()]
         
     return requested, bonus, missing
 
@@ -152,19 +154,20 @@ def organize_found_songs(wanted_songs: List[str], found_songs: List[BarPlusSong]
 def main(method: str, csv_path: str, strict: bool):
     # Build dict, artist -> songs
     click.echo("Compiling artist and song lists")
-    artists_songs = {}
+    artists_songs = {}  # artist.lower() -> (artist, [songs])
     if method == "csv":
         with open(csv_path, newline='', encoding="utf-8") as f:
             reader = csv.reader(f, delimiter=",")
-            fields = next(reader)  # skip field names
+            _ = next(reader)  # skip field names
+
             count = 0
             for row in reader:
-                artist = row[ARTIST].lower()
-                name = row[TRACK].lower()
-                if artist not in artists_songs:
-                    artists_songs[artist] = [name]
+                artist = row[ARTIST]
+                name = row[TRACK]
+                if artist.lower() not in artists_songs:
+                    artists_songs[artist.lower()] = (artist, [name])
                 else:
-                    artists_songs[artist].append(name)
+                    artists_songs[artist.lower()][TRACK].append(name)
                 count += 1
 
             # for key in artists_songs:
@@ -172,33 +175,33 @@ def main(method: str, csv_path: str, strict: bool):
             
             click.echo(f"Lists compiled. {len(artists_songs)} total artists, {count} total songs")
     
-    # Query loop
+    # Query loop, populate lists
     all_requested, all_bonus, all_missing = [], [], []
     with click.progressbar(artists_songs, length=len(artists_songs), label="Querying Bar+ song index") as bar:
-        for artist in bar:
-            wanted_songs = artists_songs[artist]
-            found_songs = query_songs_by_artist(artist)
+        for artist_lower in bar:
+            wanted_songs = artists_songs[artist_lower][TRACK]
+            found_songs = query_songs_by_artist(artists_songs[artist_lower][ARTIST])
             requested, bonus, missing = organize_found_songs(wanted_songs, found_songs)
             all_requested.extend(requested)
             all_bonus.extend(bonus)
-            all_missing.extend(missing)
+            all_missing.append((artists_songs[artist_lower][ARTIST], missing))
 
     # Display results (text files?)
     click.echo("Writing results to files")
     timestamp = datetime.now().strftime("%Y-%m-%dT%H-%M-%S")
-    with open(f"wanted-songs-found-{timestamp}.csv", "w", encoding="utf-8") as file:
+    with open(f"found-songs-{timestamp}.csv", "w", encoding="utf-8") as file:
         writer = csv.DictWriter(file, fieldnames=["artist", "name", "lang", "id"], lineterminator='\n')
-        writer.writeheader()
         writer.writerows(map(lambda x: asdict(x), sorted(all_requested, key=lambda x: x.artist)))
 
-    with open(f"bonus-songs-found-{timestamp}.csv", "w", encoding="utf-8") as file:
-        writer = csv.DictWriter(file, fieldnames=["artist", "name", "lang", "id"], lineterminator='\n')
-        writer.writeheader()        
-        writer.writerows(map(lambda x: asdict(x), sorted(all_bonus, key=lambda x: x.artist)))
+    if len(all_bonus) > 0 and not strict:
+        with open(f"bonus-songs-{timestamp}.csv", "w", encoding="utf-8") as file:
+            writer = csv.DictWriter(file, fieldnames=["artist", "name", "lang", "id"], lineterminator='\n')
+            writer.writerows(map(lambda x: asdict(x), sorted(all_bonus, key=lambda x: x.artist)))
 
-    with open(f"wanted-songs-missing-{timestamp}.csv", "w", encoding="utf-8") as file:
+    with open(f"missing-songs-{timestamp}.csv", "w", encoding="utf-8") as file:
         writer = csv.writer(file, lineterminator='\n')
-        writer.writerows(sorted(all_missing))
+        for artist, songs in all_missing:
+            writer.writerows([(artist, song) for song in songs])
 
     click.echo("Done!")
 
