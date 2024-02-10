1bea76ce6bcea50226437a4cb6a4fc4180e52daa
Mackenzie Frew <11793063+frewmack@users.noreply.github.com>
2024-02-09 19:45:10 -0500

    Update script to be easier to use - add requirements.txt

--- search.py
+++ search.py
@@ -4,18 +4,16 @@ A script for mass-querying the Bar+ karaoke song index based on a CSV of wanted 
 Tool for exporting Spotify playlists to CSV: https://www.spotlistr.com/export/spotify-playlist
     - Only select "Artist(s) Name" and "Track Name", and choose "," as separator
 """
+
 import os, csv, requests, re
 from dataclasses import dataclass, asdict
 from typing import Tuple, Optional, List, Dict
 from time import sleep
 from datetime import datetime
 
-from dotenv import load_dotenv
 import click
 from bs4 import BeautifulSoup
 
-
-load_dotenv()
 
 # Constants
 
@@ -24,9 +22,8 @@ TRACK = 1
 REQUEST_DELAY = 0.2
 BAR_PLUS_URL = "https://bar-plus.com/song/index"
 
-# SPOTIFY_CLIENT = spotify.Client(os.getenv("SPOTIFY_CLIENT_ID"), os.getenv("SPOTIFY_CLIENT_SECRET"))
-
 # Classes
+
 
 @dataclass
 class BarPlusSong:
@@ -38,7 +35,9 @@ class BarPlusSong:
     def dict(self):
         return {k: str(v) for k, v in asdict(self).items()}
 
+
 # Functions
+
 
 def query_bar_plus(method: str, field: str, page=1) -> str:
     """
@@ -51,7 +50,7 @@ def query_bar_plus(method: str, field: str, page=1) -> str:
         "Song[Artist]": "",
         "Song[Language]": "",
         "Song_page": page,
-        "ajax": "menu-item-grid"
+        "ajax": "menu-item-grid",
     }
 
     if method == "artist":
@@ -67,10 +66,12 @@ def query_bar_plus(method: str, field: str, page=1) -> str:
     return result
 
 
-def parse_bar_plus_html(html: str, page: int) -> Tuple[Dict[str, Tuple[str, List[BarPlusSong]]], Optional[int]]:
+def parse_bar_plus_html(
+    html: str, page: int
+) -> Tuple[Dict[str, Tuple[str, List[BarPlusSong]]], Optional[int]]:
     """
     Parses a page from the Bar+ song index.
-    
+
     Returns a dict mapping artists to BarPlusSong objects, representing the songs found on the page.
     Also returns the next page number if there is an additional page, or None if there are no more pages.
     """
@@ -83,7 +84,10 @@ def parse_bar_plus_html(html: str, page: int) -> Tuple[Dict[str, Tuple[str, List
     if len(songs) == 1 and len(songs[0]) < 4:
         # there are no songs
         return {}, None
-    songs = [BarPlusSong(song[1].strip(), song[2].strip(), song[3].strip(), song[0].strip()) for song in songs]
+    songs = [
+        BarPlusSong(song[1].strip(), song[2].strip(), song[3].strip(), song[0].strip())
+        for song in songs
+    ]
     artists_songs = {}
     for song in songs:
         if song.artist.lower() not in artists_songs:
@@ -146,7 +150,9 @@ def query_songs_by_title(title: str) -> List[BarPlusSong]:
     return all_songs
 
 
-def organize_found_songs(wanted_songs: List[str], found_songs: List[BarPlusSong]) -> Tuple[List[BarPlusSong], List[BarPlusSong], List[Tuple[str, str]]]:
+def organize_found_songs(
+    wanted_songs: List[str], found_songs: List[BarPlusSong]
+) -> Tuple[List[BarPlusSong], List[BarPlusSong], List[Tuple[str, str]]]:
     """
     Organize songs found by a certain artist.
     Return 3 lists representing:
@@ -154,7 +160,7 @@ def organize_found_songs(wanted_songs: List[str], found_songs: List[BarPlusSong]
     2. Other songs by the same artist that were found.
     3. Wanted songs that were not found.
 
-    Preconditions: 
+    Preconditions:
     - wanted_songs and found_songs are all by the same artist
     """
 
@@ -177,34 +183,55 @@ def organize_found_songs(wanted_songs: List[str], found_songs: List[BarPlusSong]
 
 
 @click.command()
-@click.option("--method", type=click.Choice(["csv", "spotify"]), required=True, help="Method for obtaining song list.")
-@click.option("--csv-path", type=click.Path(exists=True), help="Path to CSV song/artist list file")
-@click.option("--strict", is_flag=True, help="Query by song name only. Faster and potentially more accurate, but will not show other songs by same artist in results.")
-def main(method: str, csv_path: str, strict: bool):
+@click.argument(
+    "csv_path",
+    type=click.Path(exists=True),
+    required=True,
+)
+@click.option(
+    "--strict",
+    is_flag=True,
+    help="Query by song name. Faster and potentially more accurate, but will not show other songs by same artist in results.",
+)
+def main(csv_path: str, strict: bool):
+    """
+    This script bulk-searches the Bar+ Karaoke index for songs in the provided CSV_PATH.
+
+    Create a CSV file of songs you want to search from Spotify using this link: https://www.spotlistr.com/export/spotify-playlist. Make sure that you only select "Arist(s) Name" [sic] and "Track Name" as fields to include, and select "," as separator.
+
+    Note: If you want to export your Liked Songs, you can ctrl-A your Liked Songs and add to a playlist, then copy the share link for said playlist.
+
+    Works by searching for each artist in CSV_PATH, and saving all songs that were found by that artist. Creates 3 output files on success:
+
+    - found-songs-<timestamp>.csv    Songs in CSV_PATH that were found in Bar+ index.
+
+    - bonus-songs-<timestamp>.csv    Other songs whose artists were in CSV_PATH.
+
+    - missing-songs-<timestamp>.csv  Songs in CSV_PATH that were not found in Bar+ index.
+    """
     # Build dict, artist -> songs
     click.echo("Compiling artist and song lists")
     artists_songs = {}  # artist.lower() -> (artist, [songs])
-    if method == "csv":
-        with open(csv_path, newline='', encoding="utf-8") as f:
-            reader = csv.reader(f, delimiter=",")
-            _ = next(reader)  # skip field names
+    with open(csv_path, newline="", encoding="utf-8") as f:
+        reader = csv.reader(f, delimiter=",")
+        _ = next(reader)  # skip field names
 
-            count = 0
-            for row in reader:
-                artist = row[ARTIST].strip()
-                name = row[TRACK].strip()
-                if artist.lower() not in artists_songs:
-                    artists_songs[artist.lower()] = (artist, [name])
-                else:
-                    artists_songs[artist.lower()][TRACK].append(name)
-                count += 1
+        count = 0
+        for row in reader:
+            artist = row[ARTIST].strip()
+            name = row[TRACK].strip()
+            if artist.lower() not in artists_songs:
+                artists_songs[artist.lower()] = (artist, [name])
+            else:
+                artists_songs[artist.lower()][TRACK].append(name)
+            count += 1
 
-            # for key in artists_songs:
-            #     artists_songs[key].sort()
-            
-            click.echo(f"Lists compiled. {len(artists_songs)} total artists, {count} total songs")
-    elif method == "spotify":
-        pass
+        # for key in artists_songs:
+        #     artists_songs[key].sort()
+
+        click.echo(
+            f"Lists compiled. {len(artists_songs)} total artists, {count} total songs"
+        )
 
     # Query loop, populate lists
     all_requested, all_bonus, all_missing = [], [], []
@@ -214,13 +241,21 @@ def main(method: str, csv_path: str, strict: bool):
             artist, songs = tup
             all_wanted.extend([(artist, song) for song in songs])
         temp_missing = {}
-        with click.progressbar(all_wanted, length=len(all_wanted), label="Querying Bar+ song index by song title") as bar:
+        with click.progressbar(
+            all_wanted,
+            length=len(all_wanted),
+            label="Querying Bar+ song index by song title",
+        ) as bar:
             for wanted_artist, wanted_song in bar:
                 found_songs = query_songs_by_title(wanted_song)
                 wanted_found = False
                 for found_song in found_songs:
-                    if found_song.artist.lower().strip() == wanted_artist.lower().strip() \
-                            and found_song.name.lower().strip() == wanted_song.lower().strip():
+                    if (
+                        found_song.artist.lower().strip()
+                        == wanted_artist.lower().strip()
+                        and found_song.name.lower().strip()
+                        == wanted_song.lower().strip()
+                    ):
                         wanted_found = True
                         all_requested.append(found_song)
                         break
@@ -233,15 +268,21 @@ def main(method: str, csv_path: str, strict: bool):
             for a in temp_missing:
                 all_missing.append((a, temp_missing[a]))
     else:
-        with click.progressbar(artists_songs, length=len(artists_songs), label="Querying Bar+ song index by artist") as bar:
+        with click.progressbar(
+            artists_songs,
+            length=len(artists_songs),
+            label="Querying Bar+ song index by artist",
+        ) as bar:
             for artist_lower in bar:
                 wanted_songs = artists_songs[artist_lower][TRACK]
                 found_songs = query_songs_by_artist(artists_songs[artist_lower][ARTIST])
-                requested, bonus, missing = organize_found_songs(wanted_songs, found_songs)
+                requested, bonus, missing = organize_found_songs(
+                    wanted_songs, found_songs
+                )
                 all_requested.extend(requested)
                 all_bonus.extend(bonus)
                 all_missing.append((artists_songs[artist_lower][ARTIST], missing))
-    
+
     all_requested.sort(key=lambda song: (song.artist, song.name))
     all_bonus.sort(key=lambda song: (song.artist, song.name))
     all_missing.sort(key=lambda x: (x[ARTIST], x[TRACK]))
@@ -250,16 +291,24 @@ def main(method: str, csv_path: str, strict: bool):
     click.echo("Writing results to files")
     timestamp = datetime.now().strftime("%Y-%m-%dT%H-%M-%S")
     with open(f"found-songs-{timestamp}.csv", "w", encoding="utf-8") as file:
-        writer = csv.DictWriter(file, fieldnames=["artist", "name", "lang", "id"], lineterminator='\n')
-        writer.writerows(map(lambda x: asdict(x), sorted(all_requested, key=lambda x: x.artist)))
+        writer = csv.DictWriter(
+            file, fieldnames=["artist", "name", "lang", "id"], lineterminator="\n"
+        )
+        writer.writerows(
+            map(lambda x: asdict(x), sorted(all_requested, key=lambda x: x.artist))
+        )
 
     if len(all_bonus) > 0 and not strict:
         with open(f"bonus-songs-{timestamp}.csv", "w", encoding="utf-8") as file:
-            writer = csv.DictWriter(file, fieldnames=["artist", "name", "lang", "id"], lineterminator='\n')
-            writer.writerows(map(lambda x: asdict(x), sorted(all_bonus, key=lambda x: x.artist)))
+            writer = csv.DictWriter(
+                file, fieldnames=["artist", "name", "lang", "id"], lineterminator="\n"
+            )
+            writer.writerows(
+                map(lambda x: asdict(x), sorted(all_bonus, key=lambda x: x.artist))
+            )
 
     with open(f"missing-songs-{timestamp}.csv", "w", encoding="utf-8") as file:
-        writer = csv.writer(file, lineterminator='\n')
+        writer = csv.writer(file, lineterminator="\n")
         for artist, songs in all_missing:
             writer.writerows([(artist, song) for song in songs])
 
